adca5c256d2b180591801151ccbc482ef38b1b5e
J. Randall Hunt <randallhunt@gmail.com>
2017-02-26 10:24:46 -0800

    made your python look like python

--- rek_osx_tag.py
+++ rek_osx_tag.py
@@ -3,57 +3,53 @@ import os
 import subprocess
 from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option("-d", "--directory", 
-    dest="source_directory",
-    default=".",
-    help="Specify directory of photos to tag e.g. /Media/Photos/")
+def gettags(sourceimage):
+    """Returns Tags for requested image"""
+    taglist = []
+    client = boto3.client('rekognition')
 
-(options, args) = parser.parse_args()
+    print "Analyzing ", sourceimage
+    with open(sourceimage, 'rb') as image:
 
-sourceDirectory = options.source_directory
-minConfidence = 50
+        response = client.detect_labels(
+            Image={'Bytes': image.read()},
+            MaxLabels=50,
+            MinConfidence=minConfidence
+        )
+
+    for tag in response["Labels"]:
+        print tag["Confidence"], tag["Name"]
+        taglist.append(tag["Name"])
+
+    return taglist
+
+def writexattrs(F, TagList):
+    print "Setting following tags for ", F, " ", TagList
+    Result = ""
+    plistFront = '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array>'
+    plistEnd = '</array></plist>'
+    plistTagString = ''
+    for Tag in TagList:
+        plistTagString = plistTagString + '<string>{}</string>'.format(Tag.replace("'","-"))
+    TagText = plistFront + plistTagString + plistEnd
+
+    OptionalTag = "com.apple.metadata:"
+    XattrList = ["kMDItemFinderComment","_kMDItemUserTags","kMDItemOMUserTags"]
+    for Field in XattrList:
+        XattrCommand = 'xattr -w {0} \'{1}\' "{2}"'.format(OptionalTag + Field,TagText.encode("utf8"),F)
+        ProcString = subprocess.check_output(XattrCommand, stderr=subprocess.STDOUT,shell=True)
+        Result += ProcString
+    return Result
 
 if __name__ == '__main__':
-
-    def gettags(sourceimage):
-        """Returns Tags for requested image"""
-        
-        taglist = []
-        client = boto3.client('rekognition')
-
-        print "Analyzing ", sourceimage
-        with open(sourceimage, 'rb') as image:
-
-            response = client.detect_labels(
-                Image={'Bytes': image.read()},
-                MaxLabels=50,
-                MinConfidence=minConfidence
-            )
-
-        for tag in response["Labels"]:
-            print tag["Confidence"], tag["Name"]
-            taglist.append(tag["Name"])
-        
-        return taglist
-
-    def writexattrs(F,TagList):
-        print "Setting following tags for ", F, " ", TagList
-        Result = ""
-        plistFront = '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array>'
-        plistEnd = '</array></plist>'
-        plistTagString = ''
-        for Tag in TagList:
-            plistTagString = plistTagString + '<string>{}</string>'.format(Tag.replace("'","-"))
-        TagText = plistFront + plistTagString + plistEnd
-
-        OptionalTag = "com.apple.metadata:"
-        XattrList = ["kMDItemFinderComment","_kMDItemUserTags","kMDItemOMUserTags"]
-        for Field in XattrList:    
-            XattrCommand = 'xattr -w {0} \'{1}\' "{2}"'.format(OptionalTag + Field,TagText.encode("utf8"),F)
-            ProcString = subprocess.check_output(XattrCommand, stderr=subprocess.STDOUT,shell=True) 
-            Result += ProcString
-        return Result
+    parser = OptionParser()
+    parser.add_option("-d", "--directory",
+        dest="source_directory",
+        default=".",
+        help="Specify directory of photos to tag e.g. /Media/Photos/")
+    (options, args) = parser.parse_args()
+    sourceDirectory = options.source_directory
+    minConfidence = 50
 
     for fn in os.listdir(sourceDirectory):
         imagename = sourceDirectory + "/" + fn
